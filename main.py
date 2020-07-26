2a120d9f09a2f7151a9053b72df954f2ba05d5dd
Amirmehdy Zarrinnezhad <amzarrinnezhad@gmail.com>
2020-07-26 18:07:01 +0430

    getting inputs through file added

--- main.py
+++ main.py
@@ -1,19 +1,32 @@
-from array import *
+f = open('/Users/apple/Desktop/Mario.txt')
 
-n = int(input())
-m = int(input())
+n = int(f.readline())
+m = int(f.readline())
 
-sc = list(input().split())
+sc = list(f.readline().split())
 x, y = int(sc[0]), int(sc[1])
+print('x: %i, y: %i' % (x, y))
 
-k = int(input())
+k = int(f.readline())
 
 mario = dict()
 for i in range(k):
-    sc = list(input().split())
-    mario[sc[0], sc[1]] = 'red'
+    sc = list(f.readline().split())
+    mario[int(sc[0]), int(sc[1])] = 'red'
 for i in range(k):
-    sc = list(input().split())
-    mario[sc[0], sc[1]] = 'blue'
+    sc = list(f.readline().split())
+    mario[int(sc[0]), int(sc[1])] = 'blue'
+
+for line in f:
+    if (line.strip() != ''):
+        sc = list(line.split())
+        mario[int(sc[0]), int(sc[1])] = 'block'
 
 print(mario)
+
+
+# '''nxpancpanc
+# a ckadncp
+#
+# aaxax
+# '''
