555e789d9ec021fd3cf513f060e05e8001427d83
Amirmehdy Zarrinnezhad <amzarrinnezhad@gmail.com>
2020-08-03 07:14:28 +0430

    final_print function added and Class State created

--- main.py
+++ main.py
@@ -1,29 +1,56 @@
-f = open('/Users/apple/Desktop/Mario.txt')
+class State:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.bigH = None
+
+    def bigH_update(self, m):
+        self.bigH = min(self.bigH, m)
+
+
+f = open('/Users/apple/Desktop/givenMap.txt')
 
 n = int(f.readline())
 m = int(f.readline())
 
 sc = list(f.readline().split())
 x, y = int(sc[0]), int(sc[1])
-print('x: %i, y: %i' % (x, y))
 
 k = int(f.readline())
+reds = blues = k
 
-mario = dict()
+blueBool = redBool = False
+blueSteps = redSteps = 0
+
+givenMap = dict()
+
 for i in range(k):
     sc = list(f.readline().split())
-    mario[int(sc[0]), int(sc[1])] = 'red'
+    givenMap[int(sc[0]), int(sc[1])] = 'red'
 for i in range(k):
     sc = list(f.readline().split())
-    mario[int(sc[0]), int(sc[1])] = 'blue'
+    givenMap[int(sc[0]), int(sc[1])] = 'blue'
 
 for line in f:
-    if (line.strip() != ''):
+    if line.strip() != '':
         sc = list(line.split())
-        mario[int(sc[0]), int(sc[1])] = 'block'
+        givenMap[int(sc[0]), int(sc[1])] = 'block'
+print(givenMap)
 
-print(mario)
+stepNums = 0
+remaining = 2 * k
 
+
+def final_print():
+    print('Number of steps to reach the first RED mushroom: ', redSteps)
+    print('Number of steps to reach the first BLUE mushroom: ', blueSteps)
+
+
+while (True):
+
+    if redBool & blueBool:
+        final_print()
+        break
 
 # '''nxpancpanc
 # a ckadncp
